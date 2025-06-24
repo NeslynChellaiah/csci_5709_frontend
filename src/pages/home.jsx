9c6bfdba4bd6d3fd3666e410938709a7bec6d3b7
Abu kalam Babuji <abukalambabuji99@gmail.com>
2025-06-24 19:13:07 -0300

    Feature : filter Bar in home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,22 @@
-
+import FilterSidebar from '../components/filterBar';
 const Home = () => {
-    return (<div>
-        test
-    </div>);
+    return (
+        <main className="pt-16">
+            <div className="flex">
+                {/* Sidebar */}
+                <div className="w-80 p-6">
+                <FilterSidebar />
+                </div>
+
+                {/* Main content */}
+                <div className="flex-1 p-8">
+                <h2 className="text-xl font-semibold mb-4">Search Results</h2>
+                The restaurant cards can be placed here
+                </div>
+            </div>
+        </main>
+
+      );
 }
 
 export default Home;
