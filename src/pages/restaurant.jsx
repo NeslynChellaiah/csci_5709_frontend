678eafbc5a8e03b6f48927296441b8060cb7667b
Sivarajesh Balamurali <b.sivarajesh20@gmail.com>
2025-07-14 13:51:14 -0300

    feat(frontend): Google Map API integration for Restaurant.

--- src/pages/restaurant.jsx
+++ src/pages/restaurant.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ImageCarousel from '../components/imgCarousel'
 import Reservation from '../components/reservation'
+import RestaurantMap from '../components/restaurantMap'
 
 const Restaurant = () => {
   return (
@@ -8,6 +9,7 @@ const Restaurant = () => {
       <div className="overflow-y-auto max-h-[calc(100vh-4rem)] pr-2">
         <ImageCarousel />
         <Reservation />
+        <RestaurantMap latitude={44.638452391512345} longitude={-63.590358497425484} />
       </div>
     </div>
   )
