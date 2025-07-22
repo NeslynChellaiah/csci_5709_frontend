37ca2964c7234c624df3ed4db57c9fcde64e1319
Neslyn <neslynchellaiah@gmail.com>
2025-07-21 23:51:16 -0300

    feat/nc-add_review

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 export const BASE_URL="https://dineconnect-backend-latest.onrender.com/api";
 export const getToken = () => {
-    return "Bearer "+"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzYWt0aGlzaGFyYW5tQGdtYWlsLmNvbSIsImlhdCI6MTc1Mjg5OTY4MSwiZXhwIjoxNzUyOTAzMjgxfQ.puXy5_edqKghO5kahHwxRGzeLTISSrWAD39dtIWXelAI6WyTOPLu-FbWJYuLFDFHLfJ5_KZgvQfzj6JkW3W_3A";
+    return "Bearer "+"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbkBkYWwuY2EiLCJpYXQiOjE3NTMxNTA5OTcsImV4cCI6MTc1MzE1NDU5N30.q3bq7uP_JmG01XryyuyA-zlIR_2pvrJOEYhfVeUP7Y85n0SmxrtfRosD3z9ZVsA_3zUqxK6lprlr73aPl701cQ";
 }
